0fe66c1f76af8b6eb9bad5fe861ca12bd75ed4f2
Nuvem <Steph7@users.noreply.github.com>
2025-11-07 22:59:44 +0000

    atualizar cliente

--- cliente_modpbus.py
+++ cliente_modpbus.py
@@ -9,18 +9,23 @@ PORT = 502
 client = ModbusTcpClient(ESP32_IP, port=PORT)
 
 # Endereços dos registradores
-REG_VELOCIDADE = 0
-REG_POSICAO_FRENTE = 1
-REG_POSICAO_TRAS = 2
+REG_DISTANCIA_FRENTE = 0;  
+REG_DISTANCIA_TRAS = 1;    
+REG_DIRECAO = 2;        
+REG_VELOCIDADE = 3;   
+REG_TEMPO = 4;             
+REG_COMANDO = 5;           
+REG_ERRO = 6;              
 
 try:
     while True:
-        # Lê os registradores holding (1 registrador cada)
+        # Ler os registradores holding
         vel = client.read_holding_registers(REG_VELOCIDADE, 1).registers[0]
         frente = client.read_holding_registers(REG_POSICAO_FRENTE, 1).registers[0]
         tras = client.read_holding_registers(REG_POSICAO_TRAS, 1).registers[0]
+        direcao = client.read_holding_registers(REG_DIRECAO, 1).registers[0]
 
-        print(f"Velocidade: {vel}, Frente: {frente}, Tras: {tras}")
+        print(f"Velocidade: {vel}, Dist_Frente: {frente}, Dist_Tras: {tras}, Direção: {direcao} \n")
 
         time.sleep(1)  # lê a cada 1 segundo
 
